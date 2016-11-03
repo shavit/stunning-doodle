57fc4cd89d47c4efefc986f4da437686ee42300a
shavit <shavit@users.noreply.github.com>
2016-11-03 15:29:33 -0400

    ☔️

--- lib/genius_api.ex
+++ lib/genius_api.ex
@@ -1,2 +1,18 @@
 defmodule GeniusApi do
+
+  def build_url(endpoint, params) do
+    ['https://api.genius.com',
+      endpoint,
+      Enum.map(params, fn({k,v}) -> "&"<>k<>"="<>v  end)
+        |> List.to_string
+    ]
+      |> List.to_string
+      |> String.to_charlist
+  end
+
+  def request(endpoint, param_map) do
+    :inets.start
+    :ssl.start
+    :httpc.request(:get, {build_url(endpoint, param_map), []}, [], [])
+  end
 end
--- test/genius_api_test.exs
+++ test/genius_api_test.exs
@@ -2,7 +2,18 @@ defmodule GeniusApiTest do
   use ExUnit.Case
   doctest GeniusApi
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def params do
+    %{
+      "q" => "xo"
+    }
+  end
+
+  @doc """
+  Build a URL to the API endpoint with params
+  """
+  test "should build a URL" do
+    url = GeniusApi.build_url('/', params) != None
+    IO.inspect url
+
   end
 end
